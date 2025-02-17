1e435f0678de7977e9cd2d5e3e3db75a57612bf0
jan-miko <bautistajan425@gmail.com>
2025-02-16 21:02:26 -0800

    completed assignment 2 from 2/13

--- main.cpp
+++ main.cpp
@@ -28,54 +28,159 @@ template <typename T> class LinkedList {
 private:
     int length;
     Node<T> *head;
+    Node<T> *tail;
 
 public:
+    //Constructor
     LinkedList(T *value) {
+        Node<T> *newNode = new Node<T>(value);
+        this->head = newNode;
+        this->tail = newNode;
         this->length = 1;
-        this->head = new Node<T>(value);
+    }
+
+    //Destructor
+    ~LinkedList() {
+        Node<T> *temp = head;
+        while (head) {
+            head = head->next;
+            delete temp;
+            temp = head;
+        }
     }
     void add(T *value) {
         Node<T> *newNode = new Node<T>(value);
-        Node<T> *temp = head;
-        while (temp->next != NULL) {
-            temp = temp->next;
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
         }
-        temp->next = newNode;
+        else {
+            tail->next = newNode;
+            tail = newNode;
+        }
         length++;
     }
 
     void addhead(T *value) {
         Node<T> *newNode = new Node<T>(value);
-        newNode->next = head;
-        head = newNode;
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            newNode->next = head;
+            head = newNode;
+        }
         length++;
     }
 
     void delfirst() {
+        if (length == 0) {
+            return;
+        }
         Node<T> *temp = head;
-        head = head->next;
+        if (length == 1) {
+            head = nullptr;
+            tail = nullptr;
+        }
+        else {
+            head = head->next;
+        }
         delete temp;
         length--;
     }
 
     void dellast() {
+        if (length == 0) {
+            return;
+        }
         Node<T> *temp = head;
-        while (temp->next!=NULL)
-            temp = temp->next;
+        if (length ==1) {
+            head = nullptr;
+            tail = nullptr;
+        }
+        else {
+            Node<T> *pre = head;
+            while (temp->next) {
+                pre = temp;
+                temp = temp->next;
+            }
+            tail = pre;
+            tail->next = nullptr;
+        }
         delete temp;
         length--;
     }
 
     void deleteNode(int index) {
-       //TODO:Write the function to delete at the given index. Reuse the pre-written functions for edge cases. Account for missing index.
+        //TODO:Write the function to delete at the given index. Reuse the pre-written functions for edge cases. Account for missing index.
+        if (index < 0 || index >= length) { //index out of range
+            return;
+        }
+        else if (index == 0) { //index requested is the head
+            return delfirst();
+        }
+        else if (index == length - 1) { //index requested is the tail
+            return dellast();
+        }
+        else { //index is within range and is not head or tail
+            Node<T> *prev = head;
+            Node<T> *temp = head;
+            if (temp == NULL) { //can't do anything if head is null
+                return;
+            }
+            else {
+                for (int i = 0; i != index; i++) {//loops until reach desired index
+                    prev = temp;
+                    temp = temp->next;
+                }
+                if (temp != NULL) { //make sure we can call temp->next
+                    prev->next = temp->next;
+                    delete temp;
+                }
+            }
+        }
     }
 
    void insert(int index, T *value) {
-        //TODO:Write a function to insert a new node at a give index. Reuse the pre-written functions for edge cases. Account for missing index
+        //TODO:Write a function to insert a new node at a given index. Reuse the pre-written functions for edge cases. Account for missing index
+        if (index < 0 || index >= length) {
+            return;
+        }
+        else if (index == 0) {
+            addfirst(value);
+            return;
+        }
+        else if (index == length) {
+            add(value);
+            return;
+        }
+        else {
+            Node<T> *newNode = new Node<T>(value);
+            Node<T> *temp = head;
+            for (int i = 0; i != index - 1; i++) {//loops until reach desired index
+                temp = temp->next;
+            }
+            newNode->next = temp->next;
+            temp->next = newNode;
+            length++;
+            return;
+        }
     }
 
    void reverselist(){
         //TODO:Write a function to reverse the list using the logic from the slide.
+        Node<T>* prev, curr, next;
+        prev = NULL;
+        curr = head;
+        while(curr!=NULL) //from reversing a link list slide on canvas
+        {
+            next = curr->nextNode;
+            curr->nextNode = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
     }
 
     void print() {
@@ -100,4 +205,5 @@ int main() {
     ll->print();
     ll->dellast();
     ll->print();
+//start here
 }
